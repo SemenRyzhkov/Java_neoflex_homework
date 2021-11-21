7b520e907969bb3e007b58bfab70f10a7679affd
semen <thaisun2018@yandex.ru>
2021-11-21 12:19:54 +0300

    task3 HW2

---
+++ src/main/java/com/neoflex/hw2/task3/Seasons.java
@@ -0,0 +1,11 @@
+package com.neoflex.hw2.task3;
+
+public class Seasons {
+    public static final Seasons WINTER = new Seasons();
+    public static final Seasons SPRING = new Seasons();
+    public static final Seasons SUMMER = new Seasons();
+    public static final Seasons AUTUMN = new Seasons();
+
+    private Seasons(){
+    }
+}
